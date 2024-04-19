166efc064a2f714b3268b9e1b6668d4b6979e92d
anggasetiawanw <anggasetiaw@gmail.com>
2024-04-20 06:04:37 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/portofolio',
-  // output: 'export',
   reactStrictMode: true,
+
+  // Add basePath
+  basePath: '/portofolio',
 };
 
 module.exports = nextConfig;
